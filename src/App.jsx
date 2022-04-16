2d72b1eb4ebd03fc738331fddae45837b69bf810
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-04-16 16:21:18 +0200

    WIP Test-layout of BrowsePokemon

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,17 @@ import './App.css'
  * Divide App into smaller components (ex Routing).
  * Fixed footer at bottom.
  * Decide on fonts. 
- * Icon / images assets
+ * Icon / images assets.
  * 
  * API stuff
  *    - Pokemon states, list of pokemons (next, prev button), selected pokemon, retrieve more data from api when clicking on "i for info"-button.
  * 
  * CSS stuff
+ *    - *** make input fields shrink properly ***
  *    - Decide on colour-scheme
+ *    - "Root"-css file with default colours/fonts, import into other css-files.
  *    - Selected NavLink styling (moving pokeball?)
- *    - Pokemon "card" to display info when clicked
+ *    - Pokemon "card" to display info when clicked.
  *    - Pokemon Team grid (empty or filled with pokemon)
  *    - Browse Pokemon view - SearchBar in fitting box on top, larger box underneath with 25/50 pokemon per "next/prev"-page.
  * 
--- src/components/BrowsePokemon.jsx
+++ src/components/BrowsePokemon.jsx
@@ -4,12 +4,67 @@ import "./BrowsePokemon.css"
 
 const BrowsePokemon = () => {
 
+	const testPokemon = [
+		{ name: "bulbasaur", id: 1, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 2, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 3, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 4, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 5, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 6, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 7, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 8, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 8, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 8, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 8, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 5, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 6, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 7, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 8, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 8, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 8, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 8, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 5, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 6, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 7, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 8, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 8, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 8, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 8, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 5, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 6, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 7, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 8, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 8, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 8, picture: "[PICTURE]" },
+		{ name: "bulbasaur", id: 8, picture: "[PICTURE]" },
+	]
+
 	return (
 
-		<div>
-			<p>
-				Placeholder - Browse pokemon
-			</p>
+		<div className="container">
+
+			<section className="pokemon-search">
+				<div className="input-container">
+					<input type="text" placeholder="Search for a Pokemon" />
+					<label className="pokemon-search__icon">S</label>
+				</div>
+
+			</section>
+
+			<section className="pokemon-results">
+				<div className="test-grid-container">
+					<div className="test-grid">
+
+						{testPokemon.map(pokemon => (
+							<div className="TESTPOKEMON" key={pokemon.id}>
+								{pokemon.picture} {pokemon.name} {pokemon.id}
+							</div>
+						))}
+
+					</div>
+				</div>
+			</section>
+
 		</div>
 	)
 }
