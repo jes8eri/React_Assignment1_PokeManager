9ff9be09b690b6d8309d593f6f6f8df7bad71d76
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-04-19 15:27:45 +0200

    Searchcard styling

--- src/components/PokemonSearchCard.jsx
+++ src/components/PokemonSearchCard.jsx
@@ -6,7 +6,7 @@ const PokemonSearchCard = ({ pokemon, openPokeModal, selectedPokemon, pokemonId 
 
 	// sprite url to allow it to show up before the rest of the api-data has been fetched, alongside name
 	const spriteUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
-
+	const pokemonName = pokemon.name.substring(0, 1).toUpperCase() + pokemon.name.substring(1);
 
 	const handleOnClick = () => {
 		console.log("clicked");
@@ -18,8 +18,12 @@ const PokemonSearchCard = ({ pokemon, openPokeModal, selectedPokemon, pokemonId 
 	return (
 		<>
 			<div className="pokemon-search-card" onClick={() => handleOnClick()}>
-				<img src={spriteUrl} alt="" />
-				<p>#{pokemonId} {pokemon.name}</p>
+				<img src={spriteUrl} alt={pokemon.name} />
+				<div className="pokemon-search-card__textbox">
+					<p>{pokemonId}</p>
+					<p>{pokemonName}</p>
+				</div>
+
 
 			</div>
 
